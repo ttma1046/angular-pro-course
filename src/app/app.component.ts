5330e4caebd067abde959facc5daddc5d15e5308
Chandler Fang <chandlerfang@gmail.com>
2018-10-10 00:40:15 +1100

    27-custom-pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface File {
+    name: string;
+    size: number;
+    type: string;
+}
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+      <div>
+          <div *ngFor="let file of files">
+              <p>{{ file.name }}</p>
+              <p>{{ file.size | filesize: ' Megabytes' }}</p>
+          </div>
+      </div>
+  `,
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-demo';
+export class AppComponent implements OnInit {
   condition = false;
+  files: File[];
+
+  ngOnInit() {
+      this.files = [
+          { name: 'logo.svg', size: 2120109, type: 'image/svg' },
+          { name: 'banner.svg', size: 18029, type: 'image/jpg' },
+          { name: 'background.svg', size: 1784562, type: 'image/png' }
+      ];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UnlessDirective } from './unless.directive';
+import { FilesizePipe } from './filesize.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UnlessDirective
+    UnlessDirective,
+    FilesizePipe
   ],
   imports: [
     BrowserModule,
